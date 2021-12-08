762c71a32a050bd6838af0d01275639da8e3e52d
RafaelMoura11 <rafaelmoura7033@gmail.com>
2021-12-08 15:13:59 -0300

    Criando estrutura do context

---
+++ src/context/Provider.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import MyContext from './MyContext';
+
+function Provider({ children }) {
+  return (
+    <MyContext.Provider>
+      { children }
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.isRequired,
+};
+
+export default Provider;
