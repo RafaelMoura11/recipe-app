3bd9a7543ed08634faf1e4cd92f36e0c1e964664
Willian <willian.amorim1@outlook.com>
2021-12-08 16:28:34 -0300

    criando as rotas e as paginas

---
+++ src/context/Provider.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import MyContext from './MyContext';
+
+function Provider({ children }) {
+  return (
+    <MyContext.Provider>
+      { children }
+    </MyContext.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.isRequired,
+};
+
+// Testeee
+
+export default Provider;
