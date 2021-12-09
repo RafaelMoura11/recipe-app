ec089d1511a3d62f5811a0af0e7c4fc946a0549b
alvarogularte <alvaro.gularte@gmail.com>
2021-12-09 17:03:47 -0300

    Requisitos 67 - 69 completos

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Explore() {
   return (
     <div>
       <Header title="Explorar" enable={ false } />
+      <Link to="/explorar/comidas">
+        <h3
+          data-testid="explore-food"
+        >
+          Explorar Comidas
+        </h3>
+      </Link>
+      <Link to="/explorar/bebidas">
+        <h3
+          data-testid="explore-drinks"
+        >
+          Explorar Bebidas
+        </h3>
+      </Link>
     </div>
   );
 }
