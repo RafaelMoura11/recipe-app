6395d2fe5d168c1bed2cbb4dc677e69defe0d984
Augusto Alves <augusto.malves@yahoo.com.br>
2021-12-15 15:07:42 -0300

    finish the profile screen

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,26 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile({ history }) {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const userEmail = () => JSON.parse(localStorage.getItem('user'));
+    setEmail(userEmail);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <Header title="Perfil" enable={ false } />
-      <div>
-        Email
+      <div data-testid="profile-email">
+        {email.email}
       </div>
       <button
         type="button"
@@ -27,7 +39,7 @@ export default function Profile({ history }) {
       <button
         type="button"
         data-testid="profile-logout-btn"
-        onClick=""
+        onClick={ handleLogout }
       >
         Sair
       </button>
