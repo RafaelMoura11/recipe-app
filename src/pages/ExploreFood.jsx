cb63f8bd27a5233e92fe909f71ae9198978150fa
alvarogularte <alvaro.gularte@gmail.com>
2022-01-03 10:03:23 -0300

    Requisitos 70 - 73

--- src/pages/ExploreFood.jsx
+++ src/pages/ExploreFood.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -6,6 +7,35 @@ export default function ExploreFood() {
   return (
     <div>
       <Header title="Explorar Comidas" enable={ false } />
+      <div>
+        <Link to="/explorar/comidas/ingredientes">
+          <button
+            type="button"
+            data-testid="explore-by-ingredient"
+            name="explore-by-ingredient"
+          >
+            Por Ingredientes
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/area">
+          <button
+            type="button"
+            data-testid="explore-by-area"
+            name="explore-by-area"
+          >
+            Por Local de Origem
+          </button>
+        </Link>
+        <Link to="/explorar/comidas/surpreenda">
+          <button
+            type="button"
+            data-testid="explore-by-surprise"
+            name="explore-by-surprise"
+          >
+            Me surpreenda!
+          </button>
+        </Link>
+      </div>
       <Footer />
     </div>
   );
