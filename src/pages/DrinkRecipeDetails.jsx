5a3e7473f3605bf5bc5c20ddf1b532ee1293e783
RafaelMoura11 <rafaelmoura7033@gmail.com>
2022-01-05 14:58:07 -0300

    Adiciona a funcionalidade de favoritar receitas

--- src/pages/DrinkRecipeDetails.jsx
+++ src/pages/DrinkRecipeDetails.jsx
@@ -5,16 +5,13 @@ import { requestRecipeDetailsById, getAllIngredientsFromRecipe,
   requestRecommendedRecipes } from '../services';
 import RecommendedRecipes from '../components/RecommendedRecipes';
 import IngredientList from '../components/IngredientList';
-import shareIcon from '../images/shareIcon.svg';
+import ShareButton from '../components/ShareButton';
+import FavoriteButton from '../components/FavoriteButton';
 
-const copy = require('clipboard-copy');
-
-export default function DrinkRecipeDetails({ history,
-  location, match: { params: { id } } }) {
-  const [recipeDetails, setRecipeDetails] = useState([]);
+export default function DrinkRecipeDetails({ history, match: { params: { id } } }) {
+  const [recipeDetails, setRecipeDetails] = useState();
   const [ingredients, setIngredients] = useState([]);
   const [recommendedRecipes, setRecommendedRecipes] = useState([]);
-  const [copyed, setCopy] = useState(false);
   const [isRecipeInProgress, setIsRecipeInProgress] = useState(false);
   const [isRecipeDone, setIsRecipeDone] = useState(false);
 
@@ -48,11 +45,6 @@ export default function DrinkRecipeDetails({ history,
     history.push(`/bebidas/${id}/in-progress`);
   };
 
-  const handleShare = () => {
-    copy(`http://localhost:3000${location.pathname}`);
-    setCopy(true);
-  };
-
   const stateOfThisRecipe = () => {
     if (isRecipeDone) {
       return null;
@@ -82,33 +74,41 @@ export default function DrinkRecipeDetails({ history,
   };
 
   return (
-    <div>
-      <img
-        data-testid="recipe-photo"
-        src={ recipeDetails.strDrinkThumb }
-        alt="Recipe"
-      />
-      <h3 data-testid="recipe-title">{recipeDetails.strDrink}</h3>
-      <button
-        type="button"
-        data-testid="share-btn"
-        onClick={ handleShare }
-      >
-        <img src={ shareIcon } alt="share-icon" />
-      </button>
-      { copyed && <p>Link copiado!</p> }
-      <button type="button" data-testid="favorite-btn">Favorite</button>
-      <h4 data-testid="recipe-category">{recipeDetails.strAlcoholic}</h4>
-      <ul>
-        <IngredientList ingredients={ ingredients } />
-      </ul>
-      <p data-testid="instructions">{recipeDetails.strInstructions}</p>
-      <p data-testid="video">Video</p>
-      <div className="carousel">
-        <RecommendedRecipes recommendedRecipes={ recommendedRecipes } recipe={ false } />
+    recipeDetails ? (
+      <div>
+        <img
+          data-testid="recipe-photo"
+          src={ recipeDetails.strDrinkThumb }
+          alt="Recipe"
+        />
+        <h3 data-testid="recipe-title">{recipeDetails.strDrink}</h3>
+        <ShareButton url={ `/bebidas/${id}` } />
+        <FavoriteButton
+          id={ recipeDetails.idDrink }
+          type="bebida"
+          area=""
+          category={ recipeDetails.strCategory }
+          alcoholicOrNot={ recipeDetails.strAlcoholic }
+          name={ recipeDetails.strDrink }
+          image={ recipeDetails.strDrinkThumb }
+        />
+        <h4 data-testid="recipe-category">{recipeDetails.strAlcoholic}</h4>
+        <ul>
+          <IngredientList ingredients={ ingredients } />
+        </ul>
+        <p data-testid="instructions">{recipeDetails.strInstructions}</p>
+        <p data-testid="video">Video</p>
+        <div className="carousel">
+          <RecommendedRecipes
+            recommendedRecipes={ recommendedRecipes }
+            recipe={ false }
+          />
+        </div>
+        { stateOfThisRecipe() }
       </div>
-      { stateOfThisRecipe() }
-    </div>
+    ) : (
+      <p>Loading...</p>
+    )
   );
 }
 
@@ -118,8 +118,5 @@ DrinkRecipeDetails.propTypes = {
   }).isRequired,
   history: PropTypes.objectOf({
     push: PropTypes.func,
-  }).isRequired,
-  location: PropTypes.shape({
-    pathname: PropTypes.string,
   }).isRequired,
 };
